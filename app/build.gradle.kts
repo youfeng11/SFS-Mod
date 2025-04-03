6b41ccccea0a53d8c84c120b0c9bbfb138efdfba
由风 <youfeng11@outlook.com>
2025-04-04 00:55:43 +0800

    [Release]发布3.0.0-RC2版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 375
-        versionName = "1.5.10.5-3.0.0.RC1"
+        versionName = "1.5.10.5-3.0.0.RC2"
 
         vectorDrawables {
             useSupportLibrary = true
