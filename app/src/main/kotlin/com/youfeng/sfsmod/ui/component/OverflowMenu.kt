ee78bbbbbb018100a4212716b383fd172e11dbcb
YouFeng11 <youfeng11@outlook.com>
2025-04-18 23:07:08 +0800

    [Chore].

--- app/src/main/kotlin/com/youfeng/sfsmod/ui/component/OverflowMenu.kt
+++ app/src/main/kotlin/com/youfeng/sfsmod/ui/component/OverflowMenu.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import com.youfeng.sfsmod.R
+import com.youfeng.sfsmod.ui.viewmodel.MainViewModel
 
 /**
  * 右上角溢出菜单组件
