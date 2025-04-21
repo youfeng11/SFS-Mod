db982b306d86de91a4e5845759d2a9f788a6f0ad
YouFeng11 <youfeng11@outlook.com>
2025-04-21 17:22:03 +0800

    -

---
+++ app/src/main/kotlin/com/youfeng/sfsmod/data/model/VerifySignatureStates.kt
@@ -0,0 +1,19 @@
+package com.youfeng.sfsmod.data.model
+
+// 对比签名结果
+sealed class VerifySignatureStates {
+    /** 签名一致，正常 */
+    object SignatureValid : VerifySignatureStates()
+
+    /** 签名不匹配 */
+    object SignatureMismatch : VerifySignatureStates()
+
+    /** 获取目标APK签名的路径为空 */
+    object SignatureUnavailablePath : VerifySignatureStates()
+
+    /** 无法获取本应用的签名 */
+    object SignatureUnavailableThis : VerifySignatureStates()
+
+    /** 无法获取目标APK的签名 */
+    object SignatureUnavailableApk : VerifySignatureStates()
+}
---
+++ app/src/main/kotlin/com/youfeng/sfsmod/data/repository/MainRepository.kt
@@ -0,0 +1,48 @@
+package com.youfeng.sfsmod.data.repository
+
+import android.content.Context
+import com.youfeng.sfsmod.data.model.VerifySignatureStates
+import com.youfeng.sfsmod.utils.copyAssetFile
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okio.FileSystem
+import okio.Path
+import okio.Path.Companion.toPath
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * 数据仓库，负责文件操作，不再包含业务逻辑
+ */
+@Singleton
+class MainRepository @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    private val fileSystem = FileSystem.SYSTEM
+
+    /**
+     * 复制资源文件
+     * @return APK 临时文件路径
+     */
+    suspend fun copyResources(): Path? {
+        val dataPath = "${context.filesDir.parent}/shared_prefs/".toPath()
+        val languagePath =
+            context.getExternalFilesDir("Custom Translations")?.absolutePath?.toPath()
+        val externalCachePath = context.externalCacheDir?.absolutePath?.toPath()
+
+        // 创建目录
+        listOfNotNull(dataPath, languagePath, externalCachePath).forEach {
+            fileSystem.createDirectories(it)
+        }
+
+        // 复制资源文件
+        context.copyAssetFile(
+            "mod.xml",
+            dataPath.resolve("com.StefMorojna.SpaceflightSimulator.v2.playerprefs.xml")
+        )
+        languagePath?.let { context.copyAssetFile("translation.txt", it.resolve("简体中文.txt")) }
+        externalCachePath?.let { context.copyAssetFile("base.apk.1", it.resolve("temp.apk")) }
+
+        return externalCachePath?.resolve("temp.apk")
+    }
+
+}
