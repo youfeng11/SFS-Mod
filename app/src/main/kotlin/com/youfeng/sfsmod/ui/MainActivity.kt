026ccc21b1fff3a0f552f941469f375528487fe3
youfeng11 <youfeng11@outlook.com>
2025-08-09 23:48:32 +0800

    [Feat]在非支持的地区使用而退出前会输出Error日志了

--- app/src/main/kotlin/com/youfeng/sfsmod/ui/MainActivity.kt
+++ app/src/main/kotlin/com/youfeng/sfsmod/ui/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.youfeng.sfsmod.ui
 
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.youfeng.sfsmod.R
 import com.youfeng.sfsmod.ui.screen.MainScreen
 import com.youfeng.sfsmod.ui.theme.MainTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,14 +16,15 @@ import java.util.Locale
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(
-            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
-        )
+        enableEdgeToEdge()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             window.isNavigationBarContrastEnforced = false
         }
 
-        if (Locale.getDefault().country != "CN") finish()
+        if (Locale.getDefault().country != "CN") {
+            Log.e(getString(R.string.topbar_title), getString(R.string.not_supported_info, Locale.getDefault().country))
+            finish()
+        }
         setContent {
             MainTheme {
                 MainScreen()
